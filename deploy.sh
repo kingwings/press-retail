271c4531486b610a09c5f94aa710f88036bdfd3e
admin <admin@AWSRDLT017.aisino-wincor.com>
2020-10-18 17:40:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-# git push -f git@github.com:kingwings/kingwings.github.io.git main
+# git push -f git@github.com:kingwings/kingwings.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:kingwings/press-retail.git main:gh-pages
+git push -f git@github.com:kingwings/press-retail.git master:gh-pages
 
 cd -
